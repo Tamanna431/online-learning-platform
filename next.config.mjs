6ad07d23b0669aacd974fcdf71b197247893fe0b
Tamanna431 <tamannashuchi06@gmail.com>
2026-05-01 13:26:40 +0600

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
 };
+
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Banner from "@/components/Banner";
+import Banner from "../components/Banner";
 import { discoverValidationDepths } from "next/dist/server/app-render/instant-validation/instant-validation";
 import Image from "next/image";
 
