7d520b7b34ad5361ec0df400227f186faba6d4a3
Tamanna431 <tamannashuchi06@gmail.com>
2026-05-03 11:45:07 +0600

    google authentication set up

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,16 +1,25 @@
-import { betterAuth } from "better-auth";
-import { MongoClient } from "mongodb";
-import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { betterAuth } from "better-auth"
+import { MongoClient } from "mongodb"
+import { mongodbAdapter } from "better-auth/adapters/mongodb"
 
-const client = new MongoClient(process.env.AUTH_DB_URL);
-const db = client.db("SkillSphere");
+const client = new MongoClient(process.env.AUTH_DB_URL)
+const db = client.db("SkillSphere")
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
+  
   emailAndPassword: { 
-    enabled: true, 
-  }, 
-});
+    enabled: true,
+    requireEmailVerification: false 
+  },
+  
+  // 🔐 Google OAuth Configuration
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+})
